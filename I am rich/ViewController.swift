6fd3e3802e896fa77495abc17943dce08093828a
Rafael <rafaelvmico@gmail.com>
2023-01-11 11:49:44 +0100

    first commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Rafael Videira on 02/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
